7b34c71d877e5834f623b5ec18cdaff2eebcc904
Odls <alexeatapple@yahoo.com.tw>
2018-07-19 18:18:02 +0800

    導入 GoogleVR

---
+++ ComicTest/Assets/Scripts/FloorPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorPoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void HitLog(string p_log) {
+		Debug.Log(p_log);
+	}
+}
---
+++ ComicTest/Assets/Scripts/GvrUIPointer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GvrUIPointer : GvrBasePointer {
+	public float maxReticleDistance = 50.0f;
+	public float enterRadius;
+	public float exitRadius;
+	protected override void Start() {
+		base.Start();
+		
+	}
+
+	void Update () {
+		
+	}
+
+	public override float MaxPointerDistance { get { return maxReticleDistance; } }
+
+	public override void OnPointerEnter(RaycastResult raycastResultResult, bool isInteractive) {
+		Debug.Log("OnPointerEnter");
+	}
+
+	public override void OnPointerHover(RaycastResult raycastResultResult, bool isInteractive) {
+	}
+
+	public override void OnPointerExit(GameObject previousObject) {
+		Debug.Log("OnPointerExit");
+	}
+
+	public override void OnPointerClickDown() {}
+
+	public override void OnPointerClickUp() {}
+
+	public override void GetPointerRadius(out float p_enterRadius, out float p_exitRadius) {
+		p_enterRadius = enterRadius;
+		p_exitRadius = exitRadius;
+	}
+}
