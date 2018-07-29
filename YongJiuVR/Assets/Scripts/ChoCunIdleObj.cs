1761e4c938ceae6e472a16bcfdf419c732424db2
Odls <alexeatapple@yahoo.com.tw>
2018-07-29 05:12:10 +0100

    導入場景

--- YongJiuVR/Assets/Scripts/ChoCunIdleObj.cs
+++ YongJiuVR/Assets/Scripts/ChoCunIdleObj.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ChoCunIdleObj : MonoBehaviour {
+	public Animator anime;
 	public string name;
 	// Use this for initialization
 	void Start () {
-		
+		anime.Play (name, -1, 0);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void OnBecameInvisible () {
+		if (Random.value < 0.2f) {
+			ChoCunTop.instance.RandomChangeChoCun ();
+		}
 	}
 }
---
+++ YongJiuVR/Assets/Scripts/ChoCunTop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoCunTop : MonoBehaviour {
+	public static ChoCunTop instance;
+	public GameObject[] ChoCuns;
+
+	void Start () {
+		instance = this;
+		ChangeChoCun (0);
+	}
+
+	void Update () {
+		
+	}
+	public void RandomChangeChoCun () {
+		ChangeChoCun (Random.Range(0, ChoCuns.Length));
+	}
+	public void ChangeChoCun (int p_index) {
+		foreach (GameObject _obj in ChoCuns) {
+			_obj.gameObject.SetActive (false);
+		}
+		ChoCuns[p_index].SetActive (true);
+	}
+
+}
